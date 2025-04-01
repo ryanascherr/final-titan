86cf1978adb2bab400bf2e8aecbe61a997f5a721
ryanascherr <ryanascherr@gmail.com>
2025-03-31 23:05:36 -0400

    Added modal functionality.

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ import { placeChampions } from "./placeChampions.js";
 import { manageInputs } from "./inputCommand.js";
 import { getAndSetFilters } from "./getAndSetFilters.js";
 import { getAndSetOrder } from "./getAndSetOrder.js";
+import { openModal, closeModal } from "./modalControl.js";
 
 const supabaseURL = 'https://jjdtikulxocedonohrpf.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpqZHRpa3VseG9jZWRvbm9ocnBmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA0OTI1NjEsImV4cCI6MjA1NjA2ODU2MX0.7H56TLX1hFXqCJBgDHRU5Evj7gPtdXYUugtyPBfZQuI';
@@ -31,4 +32,19 @@ function prepareForCardPlacement() {
     array = getAndSetOrder(array);
     placeChampions(array);
 }
+
+$('body').on('click', '.champions__champion', function () {
+    let src = $(this).attr('src');
+    openModal(src);
+});
+
+$('body').on('click', '.modal', function () {
+    closeModal();
+});
+
+$('html').keyup(function(e){
+    if(e.keyCode == 8 && $(".modal").hasClass("open")){
+        closeModal();
+    }
+})
 
